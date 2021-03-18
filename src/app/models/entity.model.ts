78d0b1e9f14bd94ec0e1ddc2ea605773e1185b20
Artem Krasniuk <krasnyuk.photo@gmail.com>
2021-03-19 01:10:34 +0200

    Auto-generated commit

--- src/app/models/entity.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Entity {
-    UUID: string;
-    Number: string;
-    Date: string; // format: 2021-01-05T17:03:43
-    Carrier: string;
-    Consignee: string;
-    Status: string;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-import {Environment} from '../app/models/environment.model';
-
-export const environment: Environment = {
-  production: true,
-  apiUrl: 'https://e3ff5c3a4508.ngrok.io/ft-dev-api'
-};
